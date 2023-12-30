9dd0a9673356e5f9e61bfc5957ede2d40c9741dd
webbananaunite <mizu@webbanana.org>
2023-12-30 17:52:40 +0900

    Add Framework target.

--- blocks/blocks.h
+++ blocks/blocks.h
@@ -1,17 +1,19 @@
 //
-//  blocks.h
-//  blocks
+//  Blocks.h
+//  Blocks
 //
-//  Created by よういち on 2020/05/27.
-//  Copyright © 2020 WEB BANANA UNITE Tokyo-Yokohama LPC. All rights reserved.
+//  Created by よういち on 2023/12/30.
+//  Copyright © 2023 WEB BANANA UNITE Tokyo-Yokohama LPC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for blocks.
-FOUNDATION_EXPORT double blocksVersionNumber;
+//! Project version number for Blocks.
+FOUNDATION_EXPORT double BlocksVersionNumber;
 
-//! Project version string for blocks.
-FOUNDATION_EXPORT const unsigned char blocksVersionString[];
+//! Project version string for Blocks.
+FOUNDATION_EXPORT const unsigned char BlocksVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <blocks/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Blocks/PublicHeader.h>
+
+
